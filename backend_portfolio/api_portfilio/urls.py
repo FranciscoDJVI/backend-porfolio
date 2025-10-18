34719670c7dae9888142c925f6a1c537a2d6f197
FranciscoDJVI <vanegasfrancisco415@gmail.com>
2025-10-18 15:37:24 -0500

    correct urls

--- backend_portfolio/api_portfilio/urls.py
+++ backend_portfolio/api_portfilio/urls.py
@@ -12,14 +12,14 @@ router.register(r"api_portfilio", views.EmailViewSet, "api_portfilio")
 
 urlpatterns = [
     path("api/v1/", include(router.urls)),
-    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("api/v1/schema/", SpectacularAPIView.as_view(), name="schema"),
     path(
-        "api/schema/swagger-ui/",
+        "api/v1/schema/swagger-ui/",
         SpectacularSwaggerView.as_view(url_name="schema"),
         name="swagger-ui",
     ),
     path(
-        "api/schema/redoc/",
+        "api/v1/schema/redoc/",
         SpectacularRedocView.as_view(url_name="schema"),
         name="redoc",
     ),
