9d38fe74c944ed9456ede4328cfb30c3d9143e5c
FranciscoDJVI <vanegasfrancisco415@gmail.com>
2025-10-18 17:37:35 -0500

    fix syntax erro
    change xtand_schema for xtend_schema

--- backend_portfolio/backend_portfolio/urls.py
+++ backend_portfolio/backend_portfolio/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from drf_spectacular.utils import extand_schema
+from drf_spectacular.utils import extend_schema
 from .serializer import RootResponseSerializer
 
 
