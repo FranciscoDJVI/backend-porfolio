3dfcf6f151e5817f7351afc0dd919765e4b350dd
FranciscoDJVI <vanegasfrancisco415@gmail.com>
2025-10-18 15:50:12 -0500

    change urls

--- backend_portfolio/backend_portfolio/urls.py
+++ backend_portfolio/backend_portfolio/urls.py
@@ -1,7 +1,27 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+
+@api_view(["GET"])
+def api_root_view(request):
+    """
+    Punto de entrada principal para la API.
+    """
+    return Response(
+        {
+            "message": "Bienvenido a la API del Portfolio. Utiliza los siguientes puntos de entrada:",
+            "api_entrypoint": "/portfolio/",
+            "admin": "/admin/",
+            "documentation": "/portfolio/docs/",
+        }
+    )
+
 
 urlpatterns = [
+    path("", api_root_view),
     path("admin/", admin.site.urls),
     path("portfolio/", include("api_portfilio.urls")),
 ]
+
