8dc4cc3f4bc5591e35ef0f7454c4cfa73a392b2d
FranciscoDJVI <vanegasfrancisco415@gmail.com>
2025-10-19 22:12:15 -0500

    correct imports

--- backend_portfolio/api_portfilio/services/send_email_service.py
+++ backend_portfolio/api_portfilio/services/send_email_service.py
@@ -1,6 +1,6 @@
 from django.core.mail import send_mail
 from django.conf import settings
-from .models import Email
+from api_portfilio.models import Email
 
 
 def send_notification_email(email_id):
